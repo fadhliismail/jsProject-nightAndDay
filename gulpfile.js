aa40bb220eb709bf49e46e62be42ea6a2035d1d4
Fadhli Ismail <mfadhliismail@gmail.com>
2016-06-13 16:56:40 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+//gulp watch
+gulp.task('watch', ['browserSync', 'sass'], function(){
+    gulp.watch('app/scss/**/*/scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+//sass
+gulp.task('sass', function(){
+    return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+        stream: true
+    }))
+});
+
+//browserSync
+gulp.task('browserSync', function(){
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+});
